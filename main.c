844aba5c506c97ce82946582874f432ebb82c47f
jlee0942 <55135113+jlee0942@users.noreply.github.com>
2019-11-19 15:33:16 +0900

    04

--- main.c
+++ main.c
@@ -5,13 +5,9 @@
 
 int main(int argc, char *argv[]) {
 	
-	char src[]="The worst thigns to eat before you sleep";
-	char dst[100];
+	char str[30]="happy C programming";
 	
-	strcpy(dst, src);
-	
-	printf("copied string:%s", dst);
-	
+	printf("length of string \"%s\":%i", str, strlen(str));
 	 
 	return 0;
 }
